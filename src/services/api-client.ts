7b438d35d52df63a2c5ae7eb3213ea00c3f837e4
minkhantzaw <minkhantzaw@g.com>
2024-08-13 17:07:46 +0700

    first commit

---
+++ src/services/api-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "7555a40697ff42c1ba8ce029fb3adbf3",
+  },
+});
